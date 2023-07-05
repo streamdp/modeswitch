60c3f803b6ed6ec2b4a231b764b5bd5b7404adcf
Alexandr Primak <primak.aa@gmail.com>
2023-07-05 16:47:07 +0300

    added some testcases

---
+++ config/config_test.go
@@ -0,0 +1,112 @@
+package config
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"github.com/streamdp/modeswitch/encryption"
+	"github.com/stretchr/testify/assert"
+	"strconv"
+	"testing"
+)
+
+func TestUserConfig_Save(t *testing.T) {
+	a := app.NewWithID("com.test.testId")
+
+	testCases := []UserConfig{
+		{
+			UserName:    "username",
+			Password:    "password",
+			Host:        "192.168.100.1",
+			Port:        "22",
+			IsSsh:       true,
+			InterfaceId: "UsbLte1",
+			InitLte:     "at+xact=2,,,107",
+			InitUmts:    "at+xact=1,,,0",
+		},
+		{
+			UserName:    "username2",
+			Password:    "password2",
+			Host:        "192.168.1.1",
+			Port:        "23",
+			IsSsh:       false,
+			InterfaceId: "UsbLte0",
+			InitLte:     "at+xact=2,,,103,107",
+			InitUmts:    "at+xact=1,,,101,102",
+		},
+	}
+
+	for n, tt := range testCases {
+		tt := tt
+		t.Run(strconv.Itoa(n), func(t *testing.T) {
+			err := tt.Save(a)
+			assert.NoError(t, err)
+
+			decryptedPassword, err := encryption.Decrypt(a.Preferences().String("password"), tt.UserName)
+			assert.NoError(t, err)
+
+			assert.Equal(t, tt.UserName, a.Preferences().String("username"))
+			assert.Equal(t, tt.Password, decryptedPassword)
+			assert.Equal(t, tt.Host, a.Preferences().String("host"))
+			assert.Equal(t, tt.Port, a.Preferences().String("port"))
+			assert.Equal(t, tt.IsSsh, a.Preferences().Bool("is_ssh"))
+			assert.Equal(t, tt.InterfaceId, a.Preferences().String("interface_id"))
+			assert.Equal(t, tt.InitLte, a.Preferences().String("init_lte"))
+			assert.Equal(t, tt.InitUmts, a.Preferences().String("init_umts"))
+		})
+	}
+}
+
+func TestUserConfig_Load(t *testing.T) {
+	a := app.NewWithID("com.test.testId")
+
+	testCases := []UserConfig{
+		{
+			UserName:    "betterUsername",
+			Password:    "betterPassword",
+			Host:        "10.4.10.11",
+			Port:        "22",
+			IsSsh:       true,
+			InterfaceId: "UsbModem0",
+			InitLte:     "at+mtsm=1",
+			InitUmts:    "at+gtpkgver?",
+		},
+		{
+			UserName:    "nextUsername",
+			Password:    "securePassword",
+			Host:        "3.54.12.222",
+			Port:        "23",
+			IsSsh:       false,
+			InterfaceId: "UsbModem0",
+			InitLte:     "at@nvm:cal_usbmode.num=0",
+			InitUmts:    "at@nvm:cal_usbmode.num=10",
+		},
+	}
+
+	for n, tt := range testCases {
+		tt := tt
+		t.Run(strconv.Itoa(n), func(t *testing.T) {
+			assert.NoError(t, tt.Save(a))
+
+			c := &UserConfig{}
+			assert.NoError(t, c.Load(a))
+
+			assert.Equal(t, c.UserName, tt.UserName)
+			assert.Equal(t, c.Password, tt.Password)
+			assert.Equal(t, c.Host, tt.Host)
+			assert.Equal(t, c.Port, tt.Port)
+			assert.Equal(t, c.IsSsh, tt.IsSsh)
+			assert.Equal(t, c.InterfaceId, tt.InterfaceId)
+			assert.Equal(t, c.InitLte, tt.InitLte)
+			assert.Equal(t, c.InitUmts, tt.InitUmts)
+		})
+	}
+}
+
+func TestConstants(t *testing.T) {
+	assert.Equal(t, UMTS, "umts")
+	assert.Equal(t, LTE, "lte")
+	assert.Equal(t, Size, fyne.Size{
+		Width:  480,
+		Height: 720,
+	})
+}
---
+++ encryption/passwords_test.go
@@ -0,0 +1,121 @@
+package encryption
+
+import (
+	"github.com/stretchr/testify/assert"
+	"strconv"
+	"testing"
+)
+
+func TestBuildSecret(t *testing.T) {
+	testCases := []struct {
+		username string
+		secret   []byte
+	}{
+		{
+			username: "Alice",
+			secret:   []byte{0x41, 0x6c, 0x69, 0x63, 0x65, 0x35, 0x42, 0x55, 0x70, 0x74, 0x61, 0x4d, 0x70, 0x6b, 0x6f, 0x70, 0x61, 0x6d, 0x66, 0x6a, 0x52, 0x65, 0x35, 0x6d, 0x62, 0x53, 0x53, 0x4e, 0x64, 0x73, 0x2b, 0x55},
+		},
+		{
+			username: "Bob",
+			secret:   []byte{0x42, 0x6f, 0x62, 0x35, 0x42, 0x55, 0x70, 0x74, 0x61, 0x4d, 0x70, 0x6b, 0x6f, 0x70, 0x61, 0x6d, 0x66, 0x6a, 0x52, 0x65, 0x35, 0x6d, 0x62, 0x53, 0x53, 0x4e, 0x64, 0x73, 0x2b, 0x55, 0x30, 0x57},
+		},
+	}
+
+	for n, tt := range testCases {
+		tt := tt
+		t.Run(strconv.Itoa(n), func(t *testing.T) {
+			assert.Equal(t, tt.secret, buildSecret(tt.username))
+		})
+	}
+}
+
+func TestDecode(t *testing.T) {
+	testCases := []struct {
+		text     string
+		expected string
+	}{
+		{
+			text:     "c29tZVRleHQK",
+			expected: "someText\n",
+		},
+		{
+			text:     "YW5vdGhlck9uZQo=",
+			expected: "anotherOne\n",
+		},
+		{
+			text:     "",
+			expected: "",
+		},
+	}
+
+	for n, tt := range testCases {
+		tt := tt
+		t.Run(strconv.Itoa(n), func(t *testing.T) {
+			decoded, err := decode(tt.text)
+			assert.NoError(t, err)
+			assert.Equal(t, tt.expected, string(decoded))
+		})
+	}
+}
+
+func TestEncrypt(t *testing.T) {
+	testCases := []struct {
+		username string
+		password string
+	}{
+		{
+			username: "Alice",
+			password: "dwuMSqRwXG+9jbLy",
+		},
+		{
+			username: "Bob",
+			password: "GjIQzRFNGmFrHecT",
+		},
+	}
+
+	for n, tt := range testCases {
+		tt := tt
+		t.Run(strconv.Itoa(n), func(t *testing.T) {
+			encryptedPassword, err := Encrypt(tt.password, tt.username)
+			assert.NoError(t, err)
+			assert.NotEqual(t, tt.password, encryptedPassword)
+
+			decryptedPassword, err := Decrypt(encryptedPassword, tt.username)
+			assert.NoError(t, err)
+			assert.Equal(t, tt.password, decryptedPassword)
+		})
+	}
+}
+
+func TestDecrypt(t *testing.T) {
+	testCases := []struct {
+		username          string
+		decryptedPassword string
+		encryptedPassword string
+	}{
+		{
+			username:          "Alice",
+			decryptedPassword: "E9GTbejOsm2Egud5",
+			encryptedPassword: "tZkwWudzv5xm0DvOK3mfbxyMYQZaZdOIu+LCQWcvF1Lp1OvzJsNHaA==",
+		},
+		{
+			username:          "Bob",
+			decryptedPassword: "pA+pi9dO0dzr0+OJ",
+			encryptedPassword: "0KBG2X10HHNxDzb9FBhs7LRIoQxbrdR/ln6RP+Afocd34ExX",
+		},
+		{
+			username:          "Empty",
+			decryptedPassword: "",
+			encryptedPassword: "",
+		},
+	}
+
+	for n, tt := range testCases {
+		tt := tt
+		t.Run(strconv.Itoa(n), func(t *testing.T) {
+			decryptedPassword, err := Decrypt(tt.encryptedPassword, tt.username)
+			assert.NoError(t, err)
+			assert.Equal(t, tt.decryptedPassword, decryptedPassword)
+		})
+	}
+}
